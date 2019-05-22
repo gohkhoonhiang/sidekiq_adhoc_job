e0237cacf1de3a25c9f74f978732462e1fe572f0
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2019-05-22 20:12:34 +0800

    Fix file loading

--- lib/sidekiq_adhoc_job.rb
+++ lib/sidekiq_adhoc_job.rb
@@ -14,9 +14,11 @@ module SidekiqAdhocJob
   def self.init
     raise InvalidConfigurationError, 'Must configure before init' unless @_config
 
-    Dir[File.join(File.expand_path('sidekiq_adhoc_job', __dir__), '**/*.rb')].each { |file_name| require_relative file_name }
     Dir[File.join(File.expand_path('sidekiq_adhoc_job/utils', __dir__), '**/*.rb')].each { |file_name| require_relative file_name }
+    Dir[File.join(File.expand_path('sidekiq_adhoc_job', __dir__), '*.rb')].each { |file_name| require_relative file_name }
     Dir[File.join(File.expand_path('sidekiq_adhoc_job/services', __dir__), '**/*.rb')].each { |file_name| require_relative file_name }
+
+    SidekiqAdhocJob::WorkerFilesLoader.load
   end
 
   class Configuration
