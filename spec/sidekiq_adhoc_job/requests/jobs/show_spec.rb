ae58738506cdce8efa10438662f7203a843f2caf
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2019-05-23 15:48:05 +0800

    Spec for GET /adhoc-jobs/:name request

---
+++ spec/sidekiq_adhoc_job/requests/jobs/show_spec.rb
@@ -0,0 +1,92 @@
+require 'rack/test'
+require_relative '../../../../lib/sidekiq_adhoc_job/web/routes/jobs/show'
+
+RSpec.describe 'GET /adhoc_jobs/:name' do
+  include Rack::Test::Methods
+  include_context 'SidekiqAdhocJob setup'
+  include_context 'request setup'
+
+  context 'has arguments' do
+    it 'generates form for running job' do
+      get '/adhoc-jobs/dummy_worker'
+
+      expect(last_response.status).to eq 200
+
+      response_body = compact_html(last_response.body)
+
+      expect(response_body).to include(compact_html(
+        <<~HTML
+        <form method="POST" action="/adhoc-jobs/dummy_worker/schedule">
+        HTML
+      ))
+
+      expect(response_body).to include(compact_html(
+        <<~HTML
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="id">*id:</label>
+          <div class="col-sm-4">
+            <input class="form-control" type="text" name="id" id="id" required/>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="overwrite">*overwrite:</label>
+          <div class="col-sm-4">
+            <input class="form-control" type="text" name="overwrite" id="overwrite" required/>
+          </div>
+        </div>
+        HTML
+      ))
+
+      expect(response_body).to include(compact_html(
+        <<~HTML
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="retry_job">retry_job:</label>
+          <div class="col-sm-4">
+            <input class="form-control" type="text" name="retry_job" id="retry_job"/>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="retries">retries:</label>
+          <div class="col-sm-4">
+            <input class="form-control" type="text" name="retries" id="retries"/>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="interval">interval:</label>
+          <div class="col-sm-4">
+            <input class="form-control" type="text" name="interval" id="interval"/>
+          </div>
+        </div>
+        HTML
+      ))
+    end
+  end
+
+  context 'no argument' do
+    it 'generates form for running job' do
+      get '/adhoc-jobs/dummy_no_arg_worker'
+
+      expect(last_response.status).to eq 200
+
+      response_body = compact_html(last_response.body)
+
+      expect(response_body).to include(compact_html(
+        <<~HTML
+        <form method="POST" action="/adhoc-jobs/dummy_no_arg_worker/schedule">
+        HTML
+      ))
+
+      expect(response_body).to include('<p>No job arguments</p>')
+    end
+  end
+
+  context 'job not found' do
+    it 'redirects to index page' do
+      get '/adhoc-jobs/invalid_worker'
+
+      expect(last_response.status).to eq 302
+      expect(last_response.headers['Location']).to eq "#{last_request.base_url}/adhoc-jobs"
+    end
+  end
+
+end
