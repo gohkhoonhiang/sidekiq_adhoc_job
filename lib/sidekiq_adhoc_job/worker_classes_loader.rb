cd55d3abdae714bfa9be790cfb3ecd039770fa38
Huiming Teo <teohuiming@gmail.com>
2020-02-01 17:43:24 +0800

    Simplify some code and names

--- lib/sidekiq_adhoc_job/worker_classes_loader.rb
+++ lib/sidekiq_adhoc_job/worker_classes_loader.rb
@@ -3,9 +3,9 @@ module SidekiqAdhocJob
     @_worker_klasses = {}
 
     module ClassMethods
-      def load(module_parent_names)
+      def load(module_names)
         ObjectSpace.each_object(Class).each do |klass|
-          if worker_class?(klass) && match_module_parent_name?(klass.name, allowlist: module_parent_names)
+          if worker_class?(klass) && module_parent_name?(klass.name, allowlist: module_names)
             @_worker_klasses[worker_path_name(klass.name)] = klass
           end
         end
@@ -25,9 +25,10 @@ module SidekiqAdhocJob
           klass.included_modules.include?(Sidekiq::Worker)
         end
 
-        def match_module_parent_name?(class_name, allowlist:)
+        def module_parent_name?(class_name, allowlist:)
           allowed_parent_names = allowlist.map(&:to_s)
 
+          # allow any workers
           return true if allowed_parent_names.empty? || allowed_parent_names.include?("Module")
 
           allowed_parent_names.any? { |prefix| class_name.start_with?(prefix) }
