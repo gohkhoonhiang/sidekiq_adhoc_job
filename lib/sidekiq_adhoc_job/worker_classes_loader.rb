738b81955e57f5309a50922c5f5dc63a82ce658e
Huiming Teo <teohuiming@gmail.com>
2020-02-03 17:19:47 +0800

    Restructure to use self.method convention

--- lib/sidekiq_adhoc_job/worker_classes_loader.rb
+++ lib/sidekiq_adhoc_job/worker_classes_loader.rb
@@ -2,42 +2,40 @@ module SidekiqAdhocJob
   class WorkerClassesLoader
     @_worker_klasses = {}
 
-    module ClassMethods
-      def load(module_names)
-        ObjectSpace.each_object(Class).each do |klass|
-          if worker_class?(klass) && module_parent_name?(klass.name, allowlist: module_names)
-            @_worker_klasses[worker_path_name(klass.name)] = klass
-          end
+    def self.load(module_names)
+      ObjectSpace.each_object(Class).each do |klass|
+        if worker_class?(klass) && allowed_namespace?(klass.name, allowlist: module_names)
+          @_worker_klasses[worker_path_name(klass.name)] = klass
         end
       end
-
-      def worker_klasses
-        @_worker_klasses
-      end
-
-      def find_worker_klass(path_name)
-        @_worker_klasses[path_name]
-      end
-
-      private
-
-        def worker_class?(klass)
-          klass.included_modules.include?(Sidekiq::Worker)
-        end
-
-        def module_parent_name?(class_name, allowlist:)
-          allowed_parent_names = allowlist.map(&:to_s)
-
-          # allow any workers
-          return true if allowed_parent_names.empty? || allowed_parent_names.include?('Module')
-
-          allowed_parent_names.any? { |prefix| class_name.start_with?(prefix) }
-        end
-
-        def worker_path_name(worker_name)
-          Utils::String.underscore(worker_name).gsub('/', '_')
-        end
     end
-    extend ClassMethods
+
+    def self.worker_klasses
+      @_worker_klasses
+    end
+
+    def self.find_worker_klass(path_name)
+      @_worker_klasses[path_name]
+    end
+
+    def self.worker_class?(klass)
+      klass.included_modules.include?(Sidekiq::Worker)
+    end
+    private_class_method :worker_class?
+
+    def self.allowed_namespace?(class_name, allowlist:)
+      allowed_namespaces = allowlist.map(&:to_s)
+
+      # allow any workers
+      return true if allowed_namespaces.empty? || allowed_namespaces.include?('Module')
+
+      allowed_namespaces.any? { |prefix| class_name.start_with?(prefix) }
+    end
+    private_class_method :allowed_namespace?
+
+    def self.worker_path_name(worker_name)
+      Utils::String.underscore(worker_name).gsub('/', '_')
+    end
+    private_class_method :worker_path_name
   end
 end
