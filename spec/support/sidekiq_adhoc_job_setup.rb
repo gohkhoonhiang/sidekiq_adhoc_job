c52d907fda9391b1fd7410218035d914f60eb2cd
khoaanguyenn <99160953+khoaanguyenn@users.noreply.github.com>
2023-01-17 10:57:44 +0700

    Add spec helpers

--- spec/support/sidekiq_adhoc_job_setup.rb
+++ spec/support/sidekiq_adhoc_job_setup.rb
@@ -1,16 +1,30 @@
 require_relative '../../lib/sidekiq_adhoc_job'
 
 RSpec.shared_context 'SidekiqAdhocJob setup' do
+  let(:init_first?) { true }
+
+  let(:require_confirm_worker_names) do
+    %w[
+      SidekiqAdhocJob::Test::NamespacedWorker
+      SidekiqAdhocJob::Test::SampleCSVWorker
+      SidekiqAdhocJob::Test::DummyWorker
+    ]
+  end
+  let(:module_names) { %i[SidekiqAdhocJob::Test SidekiqAdhocJob::Test::Worker] }
+  let(:strategy_name) { :default }
+
   before do
     SidekiqAdhocJob.configure do |config|
-      config.module_names = [:'SidekiqAdhocJob::Test', :'SidekiqAdhocJob::Test::Worker']
-      config.require_confirm_worker_names =
-        %w[
-          SidekiqAdhocJob::Test::NamespacedWorker
-          SidekiqAdhocJob::Test::SampleCSVWorker
-          SidekiqAdhocJob::Test::DummyWorker
-        ]
+      config.require_confirm_worker_names = require_confirm_worker_names
+      config.module_names = module_names
+      config.strategy_name = strategy_name
     end
-    SidekiqAdhocJob.init
+    SidekiqAdhocJob.init if init_first?
+  end
+
+  after do
+    SidekiqAdhocJob.instance_eval do
+      instance_variable_set(:'@_config', nil)
+    end
   end
 end
